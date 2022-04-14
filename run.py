5fe28aa9f2344c7abd760a5550c6b2fba6a2e4a2
andrewsmith002 <andrew_smith002@yahoo.ie>
2022-04-14 23:11:33 +0000

    Created option list for the user to choose from

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+import pickle
+import os
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('exam_results')
+
+print("Student Exam Record Book")
+print("Please choose from the following options:")
+
+while True:
+    d = str(input("To search for a student press {}\n" "to add student press {}\n" "to delete a student press {}\n" "to exit press {}\n" "Please enter your option here:\n".format("(s)", "(a)", "(d)","(e)")))
+ 
